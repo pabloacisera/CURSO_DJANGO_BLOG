889c901df52c17f2a733dec7ac175b35e6d86c70
pabloacisera <pabloacisera2021@gmail.com>
2024-08-20 12:17:43 -0300

    add django-tailwind_config

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "tailwind",
+    "theme",
     "blog",
+]
+
+TAILWIND_APP_NAME="theme",
+
+INTERNAL_IPS=[
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
